ad9d3d1b180720803e9f119a902739707b7951f1
schmucklishpupfe <osiemosiemosiem@gmail.com>
2024-04-27 00:53:18 +0200

    Jumping
    jumping, hitting floor and ceiling

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float gravityAcceleration;
+    public float maxFallingSpeed;
+    public float jumpAcceleration;
+    float horizontalSpeed;
+    bool isGrounded;
     void Start()
     {
         
@@ -13,6 +19,30 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(horizontalSpeed > maxFallingSpeed)
+        {
+            horizontalSpeed -= gravityAcceleration * Time.deltaTime;
+            horizontalSpeed = Mathf.Clamp(horizontalSpeed, maxFallingSpeed, jumpAcceleration);
+        }
+        if(Input.GetButtonDown("Jump"))
+        {
+            horizontalSpeed = jumpAcceleration;
+        }
+        // finalize movement
+        Vector3 move = new Vector3(0, horizontalSpeed);
+        move *= Time.deltaTime;
+        RaycastHit2D hit = Physics2D.BoxCast(transform.position + (Vector3.down * 0.25f), new Vector2(1,0.5f), 0f, Vector2.down, Mathf.Abs(move.y));
+        if(hit.collider != null && move.y < 0)
+        {
+            move.y = -hit.distance;
+            horizontalSpeed = 0;
+        }
+        RaycastHit2D hit2 = Physics2D.BoxCast(transform.position + (Vector3.up * 0.25f), new Vector2(1,0.5f), 0f, Vector2.up, Mathf.Abs(move.y));
+        if(hit2.collider != null && move.y > 0)
+        {
+            move.y = hit.distance;
+            if(!Input.GetButton("Jump"))horizontalSpeed /= 2;
+        }
+        transform.Translate(move);
     }
 }
