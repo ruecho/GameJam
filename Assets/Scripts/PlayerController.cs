3d0d0e8b8056566fc66e5d54ba0bfb6b2eb777ed
schmucklishpupfe <osiemosiemosiem@gmail.com>
2024-04-27 10:46:45 +0200

    wall collision
    blyat

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Rendering.Universal.Internal;
 using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
@@ -9,12 +10,13 @@ public class PlayerController : MonoBehaviour
     public float gravityAcceleration;
     public float maxFallingSpeed;
     public float jumpAcceleration;
-    float horizontalSpeed;
+    float gravitationalSpeed;
     bool isGrounded;
     public float runAcceleration;
+    public float changeDirAcceleration;
     public float maxSpeed;
     public float runDeceleration;
-    float verticalSpeed;
+    float movementSpeed;
     bool active = true;
     public LayerMask lm;
     //public int layer=1;
@@ -27,43 +29,83 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // jump and gravity
-        if(horizontalSpeed > maxFallingSpeed)
+        if(gravitationalSpeed > maxFallingSpeed)
         {
-            horizontalSpeed -= gravityAcceleration * Time.deltaTime;
-            horizontalSpeed = Mathf.Clamp(horizontalSpeed, maxFallingSpeed, jumpAcceleration);
+            gravitationalSpeed -= gravityAcceleration * Time.deltaTime;
+            gravitationalSpeed = Mathf.Clamp(gravitationalSpeed, maxFallingSpeed, jumpAcceleration);
         }
         if(Input.GetButtonDown("Jump") && isGrounded)
         {
-            horizontalSpeed = jumpAcceleration;
+            gravitationalSpeed = jumpAcceleration;
         }
         // walk and run
         float playerInput = Input.GetAxis("Horizontal");
-        int moveDir = playerInput > 0 ? 1 : (playerInput < 0 ? -1 : 0);
-                
+        int moveDir = playerInput > 0 ? 1 : (playerInput < 0 ? -1 : 0);      
         if(moveDir != 0)
         {
-            verticalSpeed += runAcceleration * moveDir * Time.deltaTime;
-            verticalSpeed = Mathf.Clamp(verticalSpeed, -maxSpeed, maxSpeed);
+            Debug.Log("accelerate");
+            if(movementSpeed * moveDir >= 0f)
+            {
+                Debug.Log("normal");
+                movementSpeed += runAcceleration * moveDir * Time.deltaTime;                
+            }
+            if(movementSpeed * moveDir < 0f)
+            {
+                Debug.Log("changedir");
+                movementSpeed += changeDirAcceleration * moveDir * Time.deltaTime;
+            }
+            movementSpeed = Mathf.Clamp(movementSpeed, -maxSpeed, maxSpeed);
         }
-        else
+        if(moveDir == 0)
         {
-            verticalSpeed = 0;
+            Debug.Log("decelerate");
+            if(movementSpeed > 0)
+            {
+                Debug.Log("right");
+                Debug.Log(movementSpeed);
+                movementSpeed -= runDeceleration * Time.deltaTime;
+                Debug.Log(movementSpeed);
+            }
+            if(movementSpeed < 0)
+            {
+                Debug.Log("left");
+                movementSpeed += runDeceleration * Time.deltaTime;
+            }
+            if(movementSpeed < 0.01f || movementSpeed > -0.01f)
+            {
+                Debug.Log("stopped");
+                movementSpeed = 0;
+            } 
         }
         // finalize movement
-        Vector3 move = new Vector3(verticalSpeed, horizontalSpeed);//TODO REMOVE TMP
+        Debug.Log("final move speed:" + movementSpeed);
+        Vector3 move = new Vector3(movementSpeed, gravitationalSpeed);
+        Debug.Log(move);
         move *= Time.deltaTime;
-        RaycastHit2D hit = Physics2D.BoxCast(transform.position + (Vector3.down * 0.25f), new Vector2(1,0.5f), 0f, Vector2.down, Mathf.Abs(move.y),lm);
+        RaycastHit2D hit = Physics2D.BoxCast(transform.position + (Vector3.down * 0.25f), new Vector2(0.95f,0.5f), 0f, Vector2.down, Mathf.Abs(move.y),lm);
         isGrounded = (hit.collider != null);
         if(hit.collider != null && move.y < 0)
         {
             move.y = -hit.distance;
-            horizontalSpeed = 0;
+            gravitationalSpeed = 0;
         }
-        RaycastHit2D hit2 = Physics2D.BoxCast(transform.position + (Vector3.up * 0.25f), new Vector2(1,0.5f), 0f, Vector2.up, Mathf.Abs(move.y),lm);
+        RaycastHit2D hit2 = Physics2D.BoxCast(transform.position + (Vector3.up * 0.25f), new Vector2(0.95f,0.5f), 0f, Vector2.up, Mathf.Abs(move.y),lm);
         if(hit2.collider != null && move.y > 0)
         {
-            move.y = hit.distance;
-            if(!Input.GetButton("Jump"))horizontalSpeed /= 2;
+            move.y = hit2.distance;
+            if(!Input.GetButton("Jump"))gravitationalSpeed /= 2;
+        }
+        RaycastHit2D hit3 = Physics2D.BoxCast(transform.position + (Vector3.left * 0.25f), new Vector2(0.5f,0.95f), 0f, Vector2.left, Mathf.Abs(move.x),lm);
+        if(hit3.collider != null && move.x < 0)
+        {
+            move.x = -hit3.distance;
+            movementSpeed = 0;
+        }
+        RaycastHit2D hit4 = Physics2D.BoxCast(transform.position + (Vector3.right * 0.25f), new Vector2(0.5f,0.95f), 0f, Vector2.right, Mathf.Abs(move.x),lm);
+        if(hit4.collider != null && move.x > 0)
+        {
+            move.x = -hit4.distance;
+            movementSpeed = 0;
         }
         transform.Translate(move);
         if (Input.GetKeyDown(KeyCode.O))
